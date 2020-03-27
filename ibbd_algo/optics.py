66fe6d3d1103368105b23a78ef6a23c40b7cfc22
alex <cyy0523xc@gmail.com>
2020-03-27 14:17:07 +0800

    add optics test

--- ibbd_algo/optics.py
+++ ibbd_algo/optics.py
@@ -172,6 +172,21 @@ if __name__ == "__main__":
     输出：[0 0 0 1 1]
     """
     print(labels)
+    assert max(labels) == 1 and min(labels) == 0
+
+    def distance_test(p1, p2):
+        if p1.index in [1, 2] and p2.index in [1, 2]:
+            return 10000
+        return 0
+
+    # 使用自定义距离
+    optics = Optics(4, 2, distance=distance_test)
+    optics.fit(points)
+    labels = optics.cluster(2)
+    """
+    输出：[0 0 0 0 0]
+    """
+    assert len(set(labels)) == 1 and labels[0] == 0
 
     # 构造距离矩阵
     n = len(points)
