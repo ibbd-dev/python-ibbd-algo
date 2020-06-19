80bfb0f6eeef751838f1e17ddb1383afa16ddb4b
alex <cyy0523xc@gmail.com>
2020-06-19 15:39:15 +0800

    v0.2.5: 增加匹配的最小得分设置

--- ibbd_algo/sequence.py
+++ ibbd_algo/sequence.py
@@ -39,17 +39,19 @@ class Match:
         self.seq1 = seq1
         self.seq2 = seq2
 
-    def match(self):
+    def match(self, min_score=None):
         """找到最优的匹配
         注意：匹配得到的顺序不能改变
+        :param min_score 允许匹配的最小得分，如果为None则不做判断
         :return items: list: [(idx1, idx2)]
         """
+        self.min_score = min_score
         max_score = 0
         items = np.array([])
         max_num = min(len(self.seq1), len(self.seq2))
         for num in range(1, max_num+1):
             tmp_score, tmp_items = self.match_num(num)
-            if tmp_score >= max_score:
+            if tmp_score > max_score:
                 items = tmp_items
                 max_score = tmp_score
 
@@ -100,17 +102,24 @@ class Match:
         for comb_i in comb1:
             for comb_j in comb2:
                 tmp_score = self.cal_comb_score(comb_i, comb_j)
+                if tmp_score is None:
+                    continue
                 if tmp_score >= max_score:
                     max_score = tmp_score
                     comb_match = (comb_i, comb_j)
 
         # 生成配对items
+        if comb_match is None:
+            return 0, []
+        # print(comb_match)
         items = np.array((comb_match[0], comb_match[1])).T
         return max_score, items
 
     def cal_comb_score(self, comb_i, comb_j):
         """计算集合得分"""
         scores = [self.scores[(i, j)] for i, j in zip(comb_i, comb_j)]
+        if self.min_score is not None and min(scores) < self.min_score:
+            return None
         return sum(scores)
 
 
@@ -165,3 +174,13 @@ if __name__ == '__main__':
     assert res.tolist() == []
     res = match.fmt_items(res)
     assert res == [[-1, 0], [-1, 1]]
+
+    seq1 = ['中国人民', 'abcd', '广东广州市']
+    seq2 = ['中国人', 'defijk', '广东省广州市']
+    match = Match(seq1, seq2)
+    res = match.match()
+    assert res.tolist() == [[0, 0], [1, 1], [2, 2]]
+    res = match.match(min_score=0.5)
+    assert res.tolist() == [[0, 0], [2, 2]]
+    res = match.fmt_items(res)
+    assert res == [[0, 0], [-1, 1], [1, -1], [2, 2]]
