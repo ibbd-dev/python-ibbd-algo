123f0858119f9d73bf357f7bd9bd188ce81bb10c
alex <cyy0523xc@gmail.com>
2021-04-30 10:20:34 +0800

    add test

---
+++ ibbd_algo/sequence_test.py
@@ -0,0 +1,20 @@
+'''
+sequence测试
+
+Author: alex
+Created Time: 2021年04月30日 星期五 10时01分47秒
+'''
+import sys
+import time
+import json
+from sequence import Match, text_score
+
+with open(sys.argv[1], encoding='utf8') as f:
+    s_lines, d_lines = json.load(f)
+
+window = 1
+start = time.time()
+match = Match(s_lines, d_lines, score_func=lambda l1, l2: text_score(l1, l2, min_text_len=5), window=window)
+print('===> Match init: ', time.time()-start, flush=True)
+matches = match.match(min_score=0.1, debug=False)
+print('===> Match Page: %d' % len(matches), time.time()-start, flush=True)
